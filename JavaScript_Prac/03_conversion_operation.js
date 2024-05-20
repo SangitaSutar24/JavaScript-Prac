1ce5c496fe7a8ab766ae9d0eaa78cb14f03107e8
Sangita Sutar <64782340+SangitaSutar24@users.noreply.github.com>
2024-05-20 10:33:52 +0000

    operation

--- JavaScript_Prac/03_conversion_operation.js
+++ JavaScript_Prac/03_conversion_operation.js
@@ -28,3 +28,40 @@ let StringNumber = String(someNumber)
 
 console.log(typeof StringNumber);
 console.log(StringNumber);
+
+//********************* Operation *********************//
+
+let value = 3
+let negValue = -value
+
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(3**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello "
+let str2 = "Sangita"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + "2" + 2);
+
+// console.log((3 + 4) * 5 % 8);
+
+console.log(+true);
+console.log(+"");
+ 
+
+let gamecounter = 100
+gamecounter++
+++gamecounter
+console.log(gamecounter);
