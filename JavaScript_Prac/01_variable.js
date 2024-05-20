a990f3e7e93f44de62248cd8ecfc77563bbdb986
Sangita Sutar <64782340+SangitaSutar24@users.noreply.github.com>
2024-05-20 03:53:08 +0000

    datatypes

--- JavaScript_Prac/01_variable.js
+++ JavaScript_Prac/01_variable.js
@@ -1,13 +1,16 @@
 const accountId = 42321
 let accountEmail = "abc@gmail.com"
 
-// Prefer not to use var because of issue in block scope and functional scope.
+/* Prefer not to use var
+because of issue in block scope and functional scope. */
+
 var accountPassword = "22123"
 accountCity = "Bhubaneswar"
 
 let accountState;
 
-// accountId = 235542 //it didn't take new value bcz it is const. so it is not change. It have fixed value.
+/* accountId = 235542 //it didn't take new value bcz it is const.
+ so it is not change. It have fixed value. */
 
 accountEmail = "dcv@gmail.com"
 accountPassword = "11231"
---
+++ JavaScript_Prac/02_dataType.js
@@ -0,0 +1,14 @@
+console.log("Sangita");
+
+// number => 2 to power of 53
+// bigInt
+// String => ""
+// boolean => True/False
+// null =>Stanalone value
+// undefine 
+// Symbol => unique
+
+// object
+
+console.log(typeof undeined); // o/p:- undeined
+console.log(typeof null); // o/p:- object
