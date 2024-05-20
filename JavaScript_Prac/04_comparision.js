6e78104b57c404fc0b930303432965ae60455b45
Sangita Sutar <64782340+SangitaSutar24@users.noreply.github.com>
2024-05-20 11:37:53 +0000

    comparision

---
+++ JavaScript_Prac/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+
+console.log(undefined > 0);
+console.log(undefined >= 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+
+//=== => it strickly check the value also value's dataType.
+
+console.log("2" === 2);
