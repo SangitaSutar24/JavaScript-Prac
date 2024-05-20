161ea6461c0e34639571ae8220ed65e5b6ee9053
Sangita Sutar <64782340+SangitaSutar24@users.noreply.github.com>
2024-05-20 06:38:09 +0000

    conversion

---
+++ JavaScript_Prac/03_conversion_operation.js
@@ -0,0 +1,30 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let ValueIntNumber = Number(score)
+console.log(typeof ValueIntNumber);
+console.log(ValueIntNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 & false => 0
+
+//let isLogedIn = 1
+let isLogedIn = " "
+
+let BooleanIsLogedIn = Boolean(isLogedIn)
+
+console.log(typeof BooleanIsLogedIn);
+console.log(BooleanIsLogedIn);
+
+// 1 => true & 0 => false
+// "" => false 
+// "Sangita" => true & " " => true
+
+let someNumber = 33
+let StringNumber = String(someNumber)
+
+console.log(typeof StringNumber);
+console.log(StringNumber);
