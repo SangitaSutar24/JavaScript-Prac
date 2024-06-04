91da81cdcaeaa17dffb98461a60714d461bf4a60
Sangita Sutar <64782340+SangitaSutar24@users.noreply.github.com>
2024-06-04 06:24:00 +0000

    math&number

---
+++ JavaScript_Prac/06_Math_Number.js
@@ -0,0 +1,11 @@
+const num1 = 2334;
+console.log(num1);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.897546
+console.log(otherNumber.toPrecision(5));
