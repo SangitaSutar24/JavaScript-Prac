525d0125838dae33c6b786c29291f73ff77e908d
Sangita Sutar <64782340+SangitaSutar24@users.noreply.github.com>
2024-06-04 04:59:19 +0000

    interview Ques

--- JavaScript_Prac/Interview_Ques.js
+++ JavaScript_Prac/Interview_Ques.js
@@ -6,3 +6,26 @@ function foo(a, c, ...rest){
 foo(1, 2, 3, 4, 5)
 
 // 2.
+var x = 20
+
+function foo(){
+    console.log(x);
+    var x = 10
+}
+
+foo()
+
+// 3.
+
+console.log(name)
+var name = 'Jhon'
+
+// 4.
+
+setTimeout(()=>{
+    console.log("Timeout");
+}, 0)
+Promise.resolve().then(()=>
+    console.log(Promise)
+)
+console.log("End");
