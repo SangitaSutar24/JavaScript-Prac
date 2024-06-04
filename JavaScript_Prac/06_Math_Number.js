3963ae7b2dc0dc2a2cd6e868697d01f4b5e528dc
Sangita Sutar <64782340+SangitaSutar24@users.noreply.github.com>
2024-06-04 11:20:16 +0000

    math&num

--- JavaScript_Prac/06_Math_Number.js
+++ JavaScript_Prac/06_Math_Number.js
@@ -9,3 +9,25 @@ console.log(balance.toFixed(2));
 
 const otherNumber = 123.897546
 console.log(otherNumber.toPrecision(5));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+//********************** Math **********************//
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+console.log(Math.min(4, 2, 8, 5));
+console.log(Math.max(4, 2, 8, 5));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min)
