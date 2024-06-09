31e25d0b5add18f7f16aba7819f3bac23f93b207
Sangita Sutar <64782340+SangitaSutar24@users.noreply.github.com>
2024-06-09 06:40:20 +0000

    #

---
+++ JavaScript_basic_02/01_arrays.js
@@ -0,0 +1,11 @@
+const myArr = [2, 6, 4, 8, 3, 7]
+const myNames = ["Ram", "Hari"]
+
+const myArr2 = new Array(2, 5, 7, 3, 9)
+
+console.log(myArr[0]);
+
+myArr.push(5)
+myArr.pop()
+
+console.log(myArr);
