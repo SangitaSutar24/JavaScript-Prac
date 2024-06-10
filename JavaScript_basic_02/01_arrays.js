c576181b3d002b57409c6de711ec954c19c0d727
Sangita Sutar <64782340+SangitaSutar24@users.noreply.github.com>
2024-06-10 05:12:09 +0000

    #

--- JavaScript_basic_02/01_arrays.js
+++ JavaScript_basic_02/01_arrays.js
@@ -8,4 +8,28 @@ console.log(myArr[0]);
 myArr.push(5)
 myArr.pop()
 
+myArr.unshift(9)
+myArr.shift()
+
 console.log(myArr);
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+console.log(myArr);
+console.log(typeof newArr);
+
+// Slice, Splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1, 3)
+
+console.log(myn2);
+console.log("C", myArr);
